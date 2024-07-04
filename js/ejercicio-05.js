af656865f20d8068b90fbeedb9339c881648e6f1
Facundo Castro <facundo.castrop05@gmail.com>
2024-07-04 08:53:18 -0300

    completado el ejercicio numero 5

---
+++ js/ejercicio-05.js
@@ -0,0 +1,4 @@
+// Ejercicio #5
+const valorUno = prompt ("Ingrese el numero 10.");
+const valorDos = prompt ("Ingrese el numero 15. ");
+const suma = 10 + 15; parseInt (valorUno) + parseInt (valorDos); document.write (+ suma)
