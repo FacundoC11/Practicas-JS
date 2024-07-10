5efced805b752e3533b2baeb1ccd338863ad64a2
Facundo Castro <facundo.castrop05@gmail.com>
2024-07-10 16:17:36 -0300

    finalizado el ejercicio numero 07

---
+++ js/ejercicio-07.js
@@ -0,0 +1,8 @@
+const valor1 = prompt("Ingrese el primer valor");
+const valor2 = prompt("Ingrese el segundo valor");
+const valor3 = prompt("Ingrese el tercer valor");
+if (valor3 > valor1, valor3 > valor2) {document.write(`${valor3} es mayor a ${valor1} o a ${valor2}`)}
+else {document.write (`${valor3} es menor a ${valor1} o a ${valor2}`)
+}
+
+
