29043bd10a71def06bab9007916cb5288f14a13c
Facundo Castro <facundo.castrop05@gmail.com>
2024-07-10 16:36:29 -0300

    Finalizado el ejercicio numero 08

---
+++ js/ejercicio-08.js
@@ -0,0 +1,7 @@
+const divisible = parseInt(prompt("Ingrese un valor"))
+if (divisible % 2 === 0){document.write(`el numero ${divisible} es divisible por 2`)}
+else { document.write(`el numero ${divisible} no es divisible por 2`)
+}
+
+
+
