bd7968a05fa8f02a5e56e9529f39fb0d9376dbb6
Facundo Castro <facundo.castrop05@gmail.com>
2024-07-10 15:43:47 -0300

    terminado el ejercicio numero 06

---
+++ js/ejercicio-06.js
@@ -0,0 +1,7 @@
+const valor1 = prompt("Ingrese un valor");
+const valor2 = prompt("Ingrese un segundo valor");
+if (valor1 > valor2) {
+  document.write(`${valor1} es mayor a ${valor2}`);
+} else {
+  document.write(`${valor1} es menor a ${valor2}`);
+}
